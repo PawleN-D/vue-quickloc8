130fac890f8b02be348eea7909cf5ce1b2bf687c
Paul <paul@databalk.africa>
2022-01-30 13:49:50 +0200

    add messages data and coordinates. Populate messages in messageView

--- src/components/MessagesView.vue
+++ src/components/MessagesView.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
         <h2>Messages</h2>
+        <div :key="data.display" v-for="data in messages">{{data.message}}</div>
     </div>
 </template>
 
 <script>
+import json from "../data/messages.json"
 export default {
     name: "Messages",
+    data() {
+        return {
+            messages: json
+        }
+    }
 }
 </script>
